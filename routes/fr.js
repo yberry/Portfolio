51bbeadd44f706dd616b3606e6bbb98a8cd116ee
Yanis <y_berry@hotmail.fr>
2018-07-22 19:48:31 +0200

    Initial commit

---
+++ routes/fr.js
@@ -0,0 +1,29 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+var mysql = require('mysql');
+var dbparameters = require('./dbparameters');
+
+var con = mysql.createConnection(dbparameters.connectionConfig);
+
+/* GET home page. */
+router.get('/', function (req, res) {
+
+    con.connect(function (err) {
+        if (err) {
+            throw err;
+        }
+
+        con.query("SELECT * FROM yberry_games", function (err, result, fields) {
+            if (err) {
+                throw err;
+            }
+
+            res.render('index', { title: 'Express', games: result });
+        });
+    })
+
+});
+
+module.exports = router;
